f98a516115cc29ff1a6591b60e5d3f36d76a0e7d
samisbakedham <samsafahi@gmail.com>
2022-03-17 17:34:28 -0700

    add files

---
+++ app/view/src/components/modules/coTrade/echarts/assetsChart.vue
@@ -0,0 +1,16 @@
+<template>
+  <div style="height: 100%">
+    <div id="assetsEcharts" class="echart-box"></div>
+    </div>
+</template>
+<script>
+import mixin from 'BlockChain-ui-privatization/PC/common-mixin/modules/coTrade/echarts/assetsChart';
+import 'BlockChain-ui-privatization/PC/common-mixin/modules/coTrade/echarts/assetsChart.styl';
+
+export default {
+  mixins: [mixin],
+  mounted() {
+    this.init();
+  },
+};
+</script>
---
+++ app/view/src/views/otcTrade/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <component v-bind:is="templateLayoutType"></component>
+</template>
+
+<script>
+import mixin from 'BlockChain-ui-privatization/PC/common-mixin/otcTrade/index/index';
+import 'BlockChain-ui-privatization/PC/common-mixin/otcTrade/otcTrade.styl';
+import China from './china.vue';
+import International from './international.vue';
+
+export default {
+  mixins: [mixin],
+  components: {
+    International,
+    China,
+  },
+};
+</script>
